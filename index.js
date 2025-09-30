a99f3c8fcff4e568f4a4b78f85da77e0c13e9c0b
arthurbgrs <arthur.f.borges6@aluno.senai.br>
2025-09-30 17:19:12 -0300

    foi ai

--- index.js
+++ index.js
@@ -9,18 +9,17 @@ const caminho = path.join(__dirname, "views");
 
 // importações
 // importa as rotas de usuario
-const userRoutes = require("./routes/userRoutes")
+const userRoutes = require("./routes/userRoutes");
 
-const produtoRoutes = require("./routes/produtoRoutes")
-
+const produtoRoutes = require("./routes/produtoRoutes");
 
 // interpretador de json, para tratar as informações do body
-app.use(express.urlencoded({extend:true}))
-app.use(express.json())
+app.use(express.urlencoded({ extend: true }));
+app.use(express.json());
 // cria uma rota para as sub rotas de usuario
-app.use("/usuarios", userRoutes)
+app.use("/usuarios", userRoutes);
 
-app.use("/produtos", produtoRoutes)
+app.use("/produtos", produtoRoutes);
 
 //defininfo o ejs como template engine
 app.set("view engine", "ejs");
@@ -29,17 +28,18 @@ app.set("views", path.join(__dirname, "views"));
 //página de home do site
 app.get("/home", (req, res) => {
   res.status(200);
-  res.render("index");
-});
-//levar para a página de erro
-app.use((req, res) => {
-  res.status(404);
-  res.render("404");
+  res.render("index", { titulo: "página inicial" });
 });
 
 //rota inicial do projeto
 app.get("/", (req, res) => {
-  res.status(200).send("Olá, parabéns conseguiu");
+  res.status(200).render("index", { titulo: "página inicial" });
+});
+
+//levar para a página de erro
+app.use((req, res) => {
+  res.status(404);
+  res.render("404", { titulo: "página de erro"});
 });
 
 // subir o servidor
